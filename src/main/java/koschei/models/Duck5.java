893e6caccf0d8bb07e75518669a6295d488bd84a
timur <timurchic94@gmail.com>
2023-10-19 16:37:45 +0700

    git commit

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -3,7 +3,7 @@ package koschei.models;
 
 
 public class Duck5 {
-    private Egg6 egg;
+    private final Egg6 egg;
 
     public Duck5(Egg6 egg) {this.egg = egg;}
 
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -8,7 +8,7 @@ public class Egg6 {
 
     private Needle7 needle;
     @Autowired
-    public Egg6(Needle7 needle){this.needle = needle;}
+    public void setNeedle(Needle7 needle){this.needle = needle;}
 
     @Override
     public String toString() {
